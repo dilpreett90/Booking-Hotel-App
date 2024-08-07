2474a63d551c4e722021cc3cd8db4f1f9d98016c
DILPREET <dkkaur29@gmail.com>
2024-08-07 18:37:34 +0530

    added auth & auth tests & initial layout

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,16 @@
+const Footer=()=>{
+    return(
+     <div className="bg-blue-800 py-10">
+        <div className="container mx-auto flex justify-between items-center">
+            <span className="text-3xl text-white font-bold tracking-tight">
+                TouristTravel.com
+            </span>
+            <span className="text-white font-bold tracking-tight flex gap-4">
+                <p className="cursor-printer">Privacy Policy</p>
+                <p className="cursor-printer">Terms of Service</p>
+            </span>
+        </div>
+     </div>
+    );
+};
+export default Footer;
