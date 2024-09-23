c4728f6c077ed123c40a72e37db49dbd7669120c
DILPREET <dkkaur29@gmail.com>
2024-09-24 00:01:58 +0530

    Adding title and favicon

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header=()=>{
         <div className="bg-blue-800 py-6">
             <div className="container mx-auto flex justify-between">
                 <span className="text-3xl text-white font-bold tracking-tight">
-                    <Link to ="/">TBookingShooking</Link>
+                    <Link to ="/">BookingShooking</Link>
                 </span>
                 <span className="flex space-x-2">
                     {isLoggedIn ? <>
