2c35b2d1ecdeb37925fb7de317e790a758e59498
DILPREET <dkkaur29@gmail.com>
2024-09-24 18:16:24 +0530

    Cors Updates

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-    origin:"*",
+    origin:"https://bookingshooking.store",
     credentials:true,
 }))
 
